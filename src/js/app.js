51d60802f3a771b4bf551e62009e96fbb8c3caa7
Vladimir <mr.qrsant@gmail.com>
2019-04-15 19:36:29 +0300

    Правки, линтер

--- src/js/app.js
+++ src/js/app.js
@@ -6,31 +6,23 @@ const popoverTitle = dataContent.title;
 
 let clickCount = 0;
 
-dataContent.addEventListener('click', (event) => {
-  if (clickCount === 0){
+dataContent.addEventListener('click', () => {
+  if (clickCount === 0) {
     const popoperEl = document.createElement('div');
     popoperEl.className = 'popover';
-  
+
     popoperEl.innerHTML = `
       <h3 class="popoverTitle">${popoverTitle}</h3>
       <div class="popoverContent">${popoverContent}</div>`;
-  
+
     dataHolder.appendChild(popoperEl);
-  
+
     // так как добавляется после кнпоки, то придётся полностью вычитать высоту кнопки из подсказки
     popoperEl.style.marginTop = `${-dataContent.offsetHeight - popoperEl.offsetHeight - 15}px`;
-    clickCount++;
+    clickCount += 1;
   } else {
     const childToRemove = dataContent.parentNode.lastChild;
     dataContent.parentNode.removeChild(childToRemove);
-    clickCount--;
+    clickCount -= 1;
   }
 });
-
-function remove() {
-  if (this.parentNode) {
-    this.parentNode.removeChild(this);
-  }
-};
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'production',
