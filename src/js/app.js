adbda938d29a1c58d5d70a87a5abe30310f9d5d3
Vladimir <mr.qrsant@gmail.com>
2019-04-16 11:55:01 +0300

    реализация через класс

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,7 @@
-const dataHolder = document.querySelector('[data-holder=holder]');
-const dataContent = document.querySelector('[data-content]'); // cама кнопка
+import Popoper from './class_popoper';
 
-const popoverContent = dataContent.getAttribute('data-content');
-const popoverTitle = dataContent.title;
+const popoper = new Popoper();
+popoper.popoperTitle = 'Popover title';
+popoper.popoperContent = "And here's some amazing content. It's very engaging. Right?";
 
-dataContent.addEventListener('click', () => {
-  const popoperEl = document.createElement('div');
-  popoperEl.className = 'popover';
-
-  popoperEl.innerHTML = `
-    <h3 class="popoverTitle">${popoverTitle}</h3>
-    <div class="popoverContent">${popoverContent}</div>`;
-
-  dataHolder.appendChild(popoperEl);
-
-  // так как добавляется после кнпоки, то придётся полностью вычитать высоту кнопки из подсказки
-  const spaceUnderPopover = 15;
-  popoperEl.style.marginTop = `${-dataContent.offsetHeight - popoperEl.offsetHeight - spaceUnderPopover}px`;
-  popoperEl.style.marginLeft = `${(dataContent.offsetWidth - popoperEl.offsetWidth) / 2}px`;
-
-  setTimeout(() => {
-    const childToRemove = dataContent.parentNode.lastChild;
-    dataContent.parentNode.removeChild(childToRemove);
-  }, 1500);
-});
+popoper.create();
---
+++ src/js/class_popoper.js
@@ -0,0 +1,46 @@
+export default class Popoper {
+  constructor() {
+    this.type = 'button';
+    this.class = 'btn';
+    this.button = 0;
+  }
+
+  set popoperTitle(title) {
+    this.title = title;
+  }
+
+  set popoperContent(data) {
+    this.data = data;
+  }
+
+  create() {
+    this.holder = document.querySelector('[data-id=holder]');
+    this.button = document.querySelector('#buttonWithPopover');
+    this.button.setAttribute('data-content', this.data);
+
+    this.addListenerButton();
+  }
+
+  addListenerButton() {
+    this.button.addEventListener('click', () => {
+      const popoperEl = document.createElement('div');
+      popoperEl.className = 'popover';
+
+      popoperEl.innerHTML = `
+        <h3 class="popoverTitle">${this.title}</h3>
+        <div class="popoverContent">${this.data}</div>`;
+
+      this.holder.appendChild(popoperEl);
+
+      // так как добавляется после кнпоки, то придётся полностью вычитать высоту кнопки из подсказки
+      const spaceUnderPopover = 15;
+      popoperEl.style.marginTop = `${-this.button.offsetHeight - popoperEl.offsetHeight - spaceUnderPopover}px`;
+      popoperEl.style.marginLeft = `${(this.button.offsetWidth - popoperEl.offsetWidth) / 2}px`;
+
+      setTimeout(() => {
+        const childToRemove = this.button.parentElement.lastChild;
+        this.button.parentElement.removeChild(childToRemove);
+      }, 1000);
+    });
+  }
+}
